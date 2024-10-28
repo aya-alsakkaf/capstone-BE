982702204542ed7599577f96a31944950147f245
amnaalrashid <amna.alrashid@gmail.com>
2024-10-28 18:23:21 +0300

    Errors resolved

--- api/vac/vacRouters.js
+++ api/vac/vacRouters.js
@@ -4,5 +4,8 @@ const vacController = require("../vac/vacController");
 
 router.post("/", vacController.createVAC);
 router.get("/", vacController.getVACs);
+router.get("/:id", vacController.getVACById);
+router.put("/:id", vacController.updateVAC);
+router.delete("/:id", vacController.deleteVAC);
 
 module.exports = router;
